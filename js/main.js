8f1c7170b87eda9919f8854af6b4c131f12e7162
Carlos Jiménez <carlosjimenez32077@gmail.com>
2022-10-18 22:03:16 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
+
+/*--------- Navbar --------- */
+
 function hideNavbar() {
   document.getElementById("nav-mobile").classList.toggle("show");
 }
 
+/*--------- Sidebar --------- */
+
 function showSidebar() {
   document.getElementById("sidebar").classList.toggle("mobile");
+}
+
+/*--------- Section 1 --------- */
+function showModal() {
+  document.getElementById("modal-show").classList.toggle("show");
 }
 
 function showContent() {
@@ -12,25 +22,32 @@ function showContent() {
   document.getElementById("table-message").style.display = "none";
   document.getElementById("nav-tabs-top").style.display = "flex";
   document.getElementById("modal-show").classList.toggle("show");
-  document.querySelector(".section").style.display = "block";
+  document.querySelector(".section").style.display = "flex";
 }
+
+function hideModal() {
+  document.getElementById("modal-show").classList.toggle("show");
+}
+
+/*--------- Section 2 --------- */
+
 function showContentView() {
   document.getElementById("table-message").style.display = "none";
   document.getElementById("nav-view-tabs-top").style.display = "flex";
   document.getElementById("modal-showView").classList.toggle("show");
   document.getElementById("tap-content").style.display = "block";
+  document.getElementById("section-alert3").style.display = "none";
 }
+
 function showCampo() {
   document.querySelector(".sub-main").style.display = "block";
   document.getElementById("nav-tabs-bottom").classList.toggle("show");
   document.getElementById("table-campo").classList.toggle("show");
   document.getElementById("campo-alert").classList.toggle("hide");
 }
-function showModal() {
-  document.getElementById("modal-show").classList.toggle("show");
-}
-function hideModal() {
-  document.getElementById("modal-show").classList.toggle("show");
+
+function showView() {
+  document.getElementById("modal-showView").classList.toggle("show");
 }
 
 function hideModalView() {
@@ -41,12 +58,37 @@ function exitModal() {
   document.getElementById("modal-show").classList.toggle("hide");
 }
 
-function showView() {
-  document.getElementById("modal-showView").classList.toggle("show");
-}
-
 function openAside() {
   document.getElementById("asideView").classList.toggle("show");
+}
+
+/*----- Section 3 -----*/
+
+function showModal3(){
+  document.getElementById("modalShow3").classList.toggle("show");
+}
+
+function hideModal3(){
+  document.getElementById("modalShow3").classList.toggle("show");
+}
+
+function showContent3(){
+  document.getElementById("section-alert3").style.display = "none";
+  document.getElementById("modalShow3").classList.toggle("show");
+}
+
+function showModalTab(){
+  document.getElementById("modalTab3").classList.toggle("show");
+}
+
+function hideModalTab3(){
+  document.getElementById("modalTab3").classList.toggle("show");
+}
+
+function showContentTab3(){
+  document.getElementById("modalTab3").classList.toggle("show");
+  document.getElementById("section3").classList.toggle("d-none");
+  document.getElementById("section-alert3").classList.toggle("d-none");
 }
 
 $(".li").click(function () {
